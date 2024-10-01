7f9942b09b2bfd4ff5aee298741052b03a080f17
C.Erdi64 <cansin.erdi@gmail.com>
2024-10-01 14:33:11 +0200

    added the script that when run shows my personal info

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Git Basics
 
 Here we is the first scrips for the basics of python
+
+# 1.1
+
+Added python-basics.py and it executes my personal information
---
+++ python-basics.py
@@ -0,0 +1,11 @@
+# 1. Define the variables
+first_name = "Cansin"  # Replace this string with your first name
+last_name = "Erdi"     # Replace this string with your last name
+birth_year = 1990      # Replace this integer with your birth year
+has_license = False     # Set to True if you have a driver's license, otherwise False
+
+# 2. Print the variable values to the console
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Birth Year:", birth_year)
+print("Has Driver's License:", has_license)
